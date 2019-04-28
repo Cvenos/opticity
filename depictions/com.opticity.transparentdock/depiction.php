f32b7d37fdd6aa9ea71672f9b5d2f30fb469c36b
cvenos <crvmaxrex@gmail.com>
2019-04-27 23:46:34 -0700

    maybe a good change idek

---
+++ depictions/com.opticity.transparentdock/depiction.php
@@ -0,0 +1,216 @@
+
+<html>
+<head>
+	<title>HapticKeyboard - TheBigBoss.org - iPhone software, apps, games, accesories, ringtones, themes, reviews</title>
+
+	<base target="_blank"/>
+
+	<link rel="stylesheet" type="text/css" href="cydia/newstyle.css"/>
+
+
+
+
+
+
+
+
+
+	<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+	<meta name ="description" content="A page about iPhone Software." />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=2.0">
+
+
+	<script type="text/javascript">
+		Window.prototype.alert=function(){};
+	</script>
+
+
+	<script type="text/javascript">
+	function moveChildren(src, dest) {
+		one = document.getElementById(src);
+		two = document.getElementById(dest);
+		while (one.hasChildNodes())
+	    	two.appendChild(one.childNodes[0]);
+	}
+	</script>
+
+
+
+
+<script type="text/javascript">
+(function() {
+     var update = function() {
+         if (window.parent != window)
+             parent.postMessage({cytyle: {name: "iframe-y", value: document.body.scrollHeight}}, "*");
+     };
+     window.addEventListener('message', function(event) {
+         var message = event.data.cytyle;
+         if (message == undefined)
+             return;
+         switch (message.name) {
+             case "iframe-y":
+                 var height = message.value;
+                 var iframes = document.getElementsByTagName("iframe");
+                 if (iframes.length != 1)
+                     return;
+                 var iframe = iframes.item(0);
+                 iframe.style.height = height + 'px';
+                 update();
+             break;
+         }
+     }, false);
+     window.addEventListener('DOMContentLoaded', update, false);
+ })();
+</script>
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-8971385472108689",
+    enable_page_level_ads: true
+  });
+</script></head>
+
+<body>
+
+
+
+	
+
+
+
+
+
+
+
+
+
+<panel>
+	<fieldset style="clear:both" id="description">
+	<label>Description</label>
+
+		<div>
+			
+Compatible with iOS 9-10<br>
+<br>
+Haptic feedback for keyboard. Devices must already support haptic feedback feature.<br>
+<br>
+Configure options from Settings.app (Settings > HapticKeyboard)<br>
+<br>
+No screenshots for this item.<br>
+
+
+
+			
+		</div>
+	</fieldset>
+
+
+	
+
+
+
+
+
+
+
+
+</panel>
+
+<script  type="text/javascript">
+ MakeImgLinks();
+</script>
+
+<center>Advertisements</center><br>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- bigboss 300x250 -->
+<ins class="adsbygoogle"
+	 style="display:inline-block;width:300px;height:250px"
+	 data-ad-client="ca-pub-2931268467240864"
+	 data-ad-slot="3724442049"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+
+
+
+
+<panel>
+
+	<fieldset style="clear:both">
+		<label>Information</label>
+		<div>
+		<table class="information" cols="2" border="0">
+		<tr>
+			<td class="key">Updated</td>
+			<td class="detail">February 7, 2017</td>
+		</tr>
+		
+		<tr>
+			<td class="key">License</td>
+			<td class="detail">Free Package</td>
+		</tr>
+		
+				
+
+		</table>
+		</div>
+	</fieldset>
+
+
+
+
+
+
+
+
+
+	<fieldset style="clear:both">
+		<a class="button" href="http://mobile.twitter.com/ca13ra1">Developer Twitter</a>
+	</fieldset>
+
+
+
+
+
+
+
+
+	<fieldset style="clear:both">
+		<a class="button" href="http://apt.thebigboss.org/developer-packages.php?name=HapticKeyboard&uuid=&ua=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.108 Safari/537.36&ip=67.1.197.1&id=67.1.197.1">Developer Packages</a>
+	</fieldset>
+
+
+
+
+
+
+<!--
+	<fieldset style="clear:both">
+		<a class="button" href="Advertise.php">Advertise in Cydia</a>
+	</fieldset>
+		
+-->	
+
+</panel>
+
+
+
+<panel>
+	<fieldset style="clear:both">
+		<a class="button" href="https://twitter.com/BigBoss">Follow @BigBoss on Twitter</a>
+	</fieldset>
+</panel>
+
+
+
+<center><a href="Terms.php">Terms and Conditions</a></center>
+<br>
+
+<center>Advertisements</center><br>
+
+
+</body>
+</html>
